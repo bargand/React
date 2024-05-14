509f4b8809b172050db4c8c4c33cfdbe5226dfe2
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-14 12:11:11 +0530

    ReactJS

--- 1. Intro To React/src/App.js
+++ 1. Intro To React/src/App.js
@@ -1,23 +1,19 @@
-import './App.css';
-import { useState } from 'react';
-import "./App.css"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0);
   return (
     <>
-    <div className="App">
-      <div className="container">
-        {value}
+      <div className="App">
+        <div className="container">{value}</div>
+        <button className="btn" id="btn1" onClick={()=>{
+          setValue(value+1)
+        }}>Increase Here</button>
+        <button className="btn" id="btn2" onClick={()=>{
+          setValue(value-1)
+        }}>Increase Here</button>
       </div>
-      <button className='btn' onClick={()=>{
-        setValue(value + 1)
-      }}>Increase Here</button>
-
-      <button className='btn' onClick={()=>{
-        setValue(value - 1)
-      }}>Decrease Here</button>
-    </div>
     </>
   );
 }
