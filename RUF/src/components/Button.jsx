3c4ad763a5486a960472189afad4fcd048c157aa
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-18 22:34:10 +0530

    React Js

--- RUF/src/components/Button.jsx
+++ RUF/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ Click }) => {
   return (
-    <div>
+    <div className="app">
       <button className="btn" onClick={Click}>
         Change Name
       </button>
