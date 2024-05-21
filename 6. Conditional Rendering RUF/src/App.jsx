d352f1193b51716301a2e5d9fa76cdc4ee182f4c
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-21 21:14:57 +0530

    React JS

---
+++ 6. Conditional Rendering RUF/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [mode, setMode] = useState("light");
+
+  let toggelMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.background = "#BD34FE";
+      document.body.style.color = "white";
+    } else {
+      setMode("light");
+      document.body.style.background = "white";
+      document.body.style.color = "#BD34FE";
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            toggelMode();
+          }}
+        >
+          Change The Mode Of Light
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
