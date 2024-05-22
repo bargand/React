e10dbd481c98d5b81ba3a85a2bc19cc03c1c8091
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-22 23:16:08 +0530

    React JS API

---
+++ 8. Exercise/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  const [value, setValue] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.restful-api.dev/objects')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setValue(data);
+      });
+  }, []);
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
