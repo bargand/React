db96aefe0378f4ec8d07dc692da374fed481a2a5
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-26 21:32:17 +0530

    React Js

--- 9. onClick and onClickCapture RUF/README.md
+++ 9. onClick and onClickCapture RUF/README.md
@@ -1,3 +1,7 @@
 ### What is Event Bubbling? 
 
 # Event Bubbling is a concept in the DOM (Document Object Model). It happens when an element receives an event, and that event bubbles up (or you can say is transmitted or propagated) to its parent and ancestor elements in the DOM tree until it gets to the root element
+
+### Stop The Event Bubbling
+
+# To use this function the event bubbling is stopped `e.stopPropagation()`
--- 9. onClick and onClickCapture RUF/src/App.jsx
+++ 9. onClick and onClickCapture RUF/src/App.jsx
@@ -5,19 +5,21 @@ function App() {
   let ParentClick = ()=>{
     alert("Parent was clicked")
   }
-  let Child1Click = ()=>{
+  let Child1Click = (e)=>{
     alert("Child1Click was clicked")
+    e.stopPropagation()
   }
-  let Child2Click = ()=>{
+  let Child2Click = (e)=>{
     alert("Child2Click was clicked")
+    e.stopPropagation()
   }
 
   return (
     <>
     <div className='center'>
-    <div className="parent" onClickCapture={ParentClick}>
-      <div className="child1" onClickCapture={Child1Click}>
-      <div className="child2" onClickCapture={Child2Click}></div>
+    <div className="parent" onClick={ParentClick}>
+      <div className="child1" onClick={Child1Click}>
+      <div className="child2" onClick={Child2Click}></div>
       </div>
     </div>
     </div>
