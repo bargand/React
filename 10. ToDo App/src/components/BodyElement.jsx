2c9bd83f1e0550f87a989115f2397bbd84b08b96
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-28 22:24:55 +0530

    React JS

--- 10. ToDo App/src/components/BodyElement.jsx
+++ 10. ToDo App/src/components/BodyElement.jsx
@@ -1,25 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./BodyElement.css";
 
 const BodyElement = () => {
-  const [todo, setTodo] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  let handleAdd=()=>{
-    setTodos([...todos, {todo, isCompleted: false}])
-    setTodo("")
-    console.log(todos)
-  }
-  let handleChange=(e)=>{
-    setTodo(e.target.value)
-  }
+  let handleAdd = () => {
+    setTodos([...todos, { todo, isCompleted: false }]);
+    setTodo("");
+    console.log(todos);
+  };
+  let handleChange = (e) => {
+    setTodo(e.target.value);
+  };
   return (
     <>
       <div className="formElement">
         <p className="input-container">
           <input
-          onChange={handleChange}
-          value={todo}
+            onChange={handleChange}
+            value={todo}
             type="text"
             placeholder="Enter your name"
             name="text"
@@ -50,14 +50,28 @@ const BodyElement = () => {
       </div>
       <div className="lowerPart">
         <h1>Your Todos</h1>
-        {todos.map(item=>{
-        return <div className="todoText">
-          <p>{item.todo}</p>
-        <div className="toButtons">
-          <button className="btn">Edit</button>
-          <button className="btn">Delete</button>
-        </div>
-        </div>
+        {todos.map((item) => {
+          return (
+            <div className="todoText">
+              <div className="todoTextFirst">
+              <label class="container">
+                <input type="checkbox" value={todo.isCompleted}/>
+                <svg viewBox="0 0 64 64" height="2em" width="2em">
+                  <path
+                    d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
+                    pathLength="575.0541381835938"
+                    class="path"
+                  ></path>
+                </svg>
+              </label>
+              <p className={item.isCompleted? "line-through":""}>{item.todo}</p>
+              </div>
+              <div className="toButtons">
+                <button className="btn">Edit</button>
+                <button className="btn">Delete</button>
+              </div>
+            </div>
+          );
         })}
       </div>
     </>
