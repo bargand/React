e40c447c3bc8b7bfe6ac194349ccf3b0f34d17cd
Debargha Nandi <debarghanandi.india@gmail.com>
2024-06-24 11:56:13 +0530

    reactjs

--- Ruf2/src/App.jsx
+++ Ruf2/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
       </div>
